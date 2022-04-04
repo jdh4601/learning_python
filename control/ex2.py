fe3da839a579bfe0fba593953bb6b1b1009a8fab
DongHyun Jung <76399021+jdh4601@users.noreply.github.com>
2022-04-04 22:47:32 +0900

    Delete control directory

--- control/ex2.py
+++
@@ -1,9 +0,0 @@
-score = 70
-
-if score >= 68:
-    message = "success"
-else:
-    message = "failure"
-    
-message = "success" if score >= 68 else "failure"
-print(message)
--- control/ex6.py
+++
@@ -1,7 +0,0 @@
-num = 0
-marks = [90, 98, 80]
-for mark in marks:
-    num += 1
-    if mark <= 70:
-        continue
-    print("%d" %num)
